2f81f03909aed0f06707c66e5d885707c041dd15
Marak <marak.squires@gmail.com>
2015-06-04 11:49:41 +0200

    [view] [api] Add ability to destroy hooks. Closes #47

--- view/hooks.js
+++ view/hooks.js
@@ -17,17 +17,24 @@ module['exports'] = function view (opts, callback) {
 
   // if there is no referral set, assign one based on the owner of the current hook
   if (typeof req.session.referredBy === "undefined") {
-    req.session.referredBy = req.hook.owner;
+    req.session.referredBy = req.params.username;
   }
+
+  // sort hooks alphabetically by name
+  opts.hooks = opts.hooks.sort(function(a,b){
+    if (a.name < b.name) return -1;
+    if (a.name > b.name) return 1;
+    return 0;
+  });
 
   for (var h in opts.hooks) {
     // TODO: add ability to delete hooks https://github.com/bigcompany/hook.io/issues/47
-    if (req.params.username.toLowerCase() === req.user.username.toLowerCase()) {
-      // $('.hooks').append('<tr><td><a href="/' + opts.hooks[h].owner + "/" + opts.hooks[h].name + '">' + opts.hooks[h].name + '</a></td><td><a href="">Delete</a></td></tr>')
+    var hookLink = "/" + opts.hooks[h].owner + "/" + opts.hooks[h].name + "";
+    if (req.user && req.params.username.toLowerCase() === req.user.username.toLowerCase()) {
+      $('.hooks').append('<tr><td class="col-md-8"><a href="' + hookLink + '">' + opts.hooks[h].name + '</a></td><td class="col-md-4" align="right"><a href="' + hookLink + '?admin=true"><span class="mega-octicon octicon-home" style="min-width: 32px;"></span></a>&nbsp;&nbsp;<a class="deleteLink" data-name="' + opts.hooks[h].owner + "/" + opts.hooks[h].name +'" href="' + hookLink + '?delete=true"><span class="mega-octicon octicon-trashcan" style="min-width: 32px;"></span></a></td></tr>')
     } else {
-      // $('.hooks').append('<tr><td><a href="/' + opts.hooks[h].owner + "/" + opts.hooks[h].name + '">' + opts.hooks[h].name + '</a></td></tr>')
+      $('.hooks').append('<tr><td><a href="' + hookLink + '">' + opts.hooks[h].name + '</a></td></tr>')
     }
-    $('.hooks').append('<tr><td><a href="/' + opts.hooks[h].owner + "/" + opts.hooks[h].name + '">' + opts.hooks[h].name + '</a></td></tr>')
   }
 
   if (typeof opts.hooks === "object") {
