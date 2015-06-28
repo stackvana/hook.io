ea2aec32a5f33bcbca8f507d6aa3ef1003e30915
Marak <marak.squires@gmail.com>
2015-06-28 06:23:39 +0200

    [refactor] [minor] Removed params.run. Always run hook on any HTTP request. Removed redundant block of hook rendering code which is now handled directly be the `hook` resource.

--- view/hook.js
+++ view/hook.js
@@ -24,95 +24,19 @@ module['exports'] = function view (opts, callback) {
     req.session.referredBy = req.hook.owner;
   }
 
-  // should we just run the hook?
-  // TODO: it looks like using ?run=true might result in template / theme logic being bypassed
-  if (params.run) {
-    opts.req = req;
-    opts.res = res;
-    return hook.runHook(opts
-    , function(err, result){
-      if (err) {
-        return res.end(err.message);
-      }
-      return callback(null, result.output);
-    });
-  }
-
-  // not forking the hook, not running the hook, we need to present it
-  var theme, presenter;
-
-  if (typeof req.hook.theme === "undefined" || req.hook.theme.length === 0) {
-    // no theme HTML
-    theme = config.defaultTheme;
-  } else {
-    theme = req.hook.theme;
-  }
-
-  if (typeof req.hook.presenter === "undefined" || req.hook.presenter.length === 0) {
-    // no theme Presenter
-    if (theme === config.defaultTheme) {
-      presenter = config.defaultPresenter;
-    } else {
-      presenter = "http://hook.io/themes/simple/index.js";
-      
-    }
-  } else {
-    presenter = req.hook.presenter;
-  }
-
   // TODO: make themes overridable through ?theme= param
   // if ?admin=true, redirect to admin page
   if (params.admin) {
     return res.redirect('/admin?owner=' + req.hook.owner + '&name=' + req.hook.name);
   }
 
-  // if a theme was set ( not the default debug theme, and no presenter was given, use simple.js )
-  // check to see if in offline mode
-  /*
-  if (big.mode === "Offline") {
-    // replace any references to http://hook.io/ production site with local dev configuration
-    theme = theme.replace('http://hook.io/', 'http://dev.hook.io:9999/')
-  }
-  */
-  hook.fetchHookTheme(theme, function(err, _theme){
+  opts.req = req;
+  opts.res = res;
+  return hook.runHook(opts, function (err, result){
     if (err) {
-      return res.end('Unable to fetch theme: ' + theme  + ' ' + err.message);
+      return res.end(err.message);
     }
-    /*
-    if (big.mode === "Offline") {
-      // replace any references to http://hook.io/ production site with local dev configuration
-      presenter = presenter.replace('http://hook.io/', 'http://dev.hook.io:9999/')
-    }
-    */
-    hook.fetchHookPresenter(presenter, function(err, _presenter){
-      if (err) {
-        return res.end(hook.formatError(err));
-      }
-
-      var _view = new View({ template: _theme.toString(), presenter: _presenter });
-
-      // give the presenter 3 seconds to render, or else it has failed
-      var completedTimer = setTimeout(function(){
-        if (!completed) {
-          return callback(new Error('Hook presenter took more than 3 seconds to load. Aborting request. \n\nA delay of this long usually means the presenter never fired it\'s callback. Check the presenter code for error. \n\nIf this is not the case and you require more than 3 seconds to present your view, please contact hookmaster@hook.io'));
-        }
-      }, 3000);
-
-      var completed = false;
-      completedTimer = clearTimeout(completedTimer);
-      try { // this will catch user run-time errors in the presenter
-        _view.present({
-          request: req,
-          response: res,
-          gist: req.hook.gist
-        }, function(err, rendered){
-          return callback(null, rendered);
-        });
-      } catch (err) {
-        return res.end(err.stack);
-      }
-
-    });
+    return res.end(result.output);
   });
 
 };
