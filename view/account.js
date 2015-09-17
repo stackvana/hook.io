ed5546bbaf27ae738e27294b4e36e90747c1dfa5
Marak <marak.squires@gmail.com>
2015-09-17 03:01:47 -0700

    [api] Adds ability to rename accounts #118 #100  * Added option for user to rename account  * Will re-key all hooks associated with account  * Will not re-key datastore or logs

--- view/account.js
+++ view/account.js
@@ -1,9 +1,11 @@
-var hook = require('../lib/resources/hook')
-var user = require('../lib/resources/user')
-var billing = require('../lib/resources/billing')
+var hook = require('../lib/resources/hook');
+var user = require('../lib/resources/user');
+var cache = require('../lib/resources/cache');
+var billing = require('../lib/resources/billing');
 var bodyParser = require('body-parser');
 var mergeParams = require('./mergeParams');
-
+var async = require('async');
+var colors = require('colors');
 // user schema used for form
 var userSchema = {
   name: { type: 'string', required: true },
@@ -23,6 +25,62 @@ module['exports'] = function view (opts, callback) {
     // TODO: actual login screen, not just homepage login
     return res.redirect('/');
   }
+
+  // TODO: refactor out most of this block using Resource.User.before() hooks
+  function renameAccount (_user, params, cb) {
+
+    // first check if new account name is available ( this should probably be a .before() hook on User.update )
+    user.find({ name: params.name }, function (err, results) {
+
+      if (err) {
+        return res.end(err.message);
+      }
+
+      if (results.length > 0) {
+        return res.end('new account name ' + params.name + ' is unavailable.');
+      }
+
+      _user.name = params.name;
+
+      user.update(_user, function(err, result){
+        if (err) {
+          return res.end(err.message);
+        }
+
+        // update user cache
+        cache.set('/user/' + params.name, result, function(){
+
+          // reset user name in session
+          // req.session.user = params.name;
+
+          // rename all existing hooks to new name
+          hook.find({ owner: params.previousName }, function (err, results) {
+            if (err) {
+              return res.end(err.message);
+            }
+            if (results.length === 0) {
+              return cb(null);
+            }
+            function updateHookName (item, cb) {
+              item.owner = params.name;
+              item.save(cb);
+            };
+            async.each(results, updateHookName, function(err){
+              if (err) {
+                return res.end(err.message);
+              }
+              return cb(null);
+            });
+          });
+        });
+
+      });
+
+      // TODO: re-key all datastore entries
+      // TODO: clear out all previous logs
+    });
+
+  };
 
   bodyParser()(req, res, function bodyParsed(){
     mergeParams(req, res, function(){});
@@ -47,11 +105,16 @@ module['exports'] = function view (opts, callback) {
         return callback(null, 'No user found');
       }
 
-      var _user = results[0];
+      var _user = {}, r = results[0];
+      _user.name = r.name;
+      _user.id = r.id;
       $('.myHooks').attr('href', '/' + _user.name);
 
       if (req.method === "POST") {
+
         // attempt to save
+        // validate email and password updates
+
         if (typeof params.email !== "undefined" && params.email.length > 3) {
           _user.email = params.email;
           if (typeof params.password !== 'undefined' && typeof params.confirmPassword !== 'undefined') {
@@ -62,35 +125,51 @@ module['exports'] = function view (opts, callback) {
               _user.password = params.password;
             }
           }
-          return user.update(_user, function(err, result){
-            if (err) {
-              return res.end(err.message);
-            }
-            // display user info in account form
-            // TODO: if form post data, attempt to update user account information
-            showUserForm(_user, function(err, result){
-              $('.userForm').html(result);
-              $('.status').html('Account Information Updated!');
-              callback(null, $.html());
+
+          // allow for account renames
+          if (typeof params.name === "undefined" || params.name.length < 3) {
+            params.name = req.session.user;
+            // return res.end('name is a required parameter!');
+          }
+
+          if (params.name && params.previousName && params.name !== params.previousName) {
+            renameAccount(_user, params, function (err, result){
+              // display user info in account form
+              // TODO: if form post data, attempt to update user account information
+              req.session.destroy();
+              req.logout();
+              res.redirect("/");
+              return;
+            })
+          } else {
+            return user.update(_user, function(err, result){
+              if (err) {
+                return res.end(err.message);
+              }
+              // display user info in account form
+              // TODO: if form post data, attempt to update user account information
+              showUserForm(_user, function(err, result){
+                $('.userForm').html(result);
+                $('.status').html('Account Information Updated!');
+                callback(null, $.html());
+              });
             });
-          });
+          }
         } else {
           return res.end('email parameter cannot be empty');
         }
       } else {
         // display user info in account form
         // TODO: if form post data, attempt to update user account information
-        showUserForm(_user, function(err, result){
+        showUserForm(r, function(err, result){
           $('.userForm').html(result);
           callback(null, $.html());
         });
       }
     });
-
   });
 
 };
-
 
 var request = require('hyperquest');
 var dateFormat = require('dateformat');
@@ -102,8 +181,15 @@ var billingForm = require('./billingForm');
 function showUserForm (user, cb) {
   var formSchema = userSchema || {};
 
-  formSchema.name.default = user.name;
-  formSchema.name.disabled = true;
+  formSchema.name = {
+    default: user.name,
+    disabled: true
+  };
+
+  formSchema.previousName = {
+    default: user.name,
+    hidden: true
+  };
 
   formSchema.email.default = user.email || "";
   // formSchema.email.disabled = true;
@@ -135,11 +221,14 @@ function showUserForm (user, cb) {
   if (typeof user.hostingCredits !== "number") {
     user.hostingCredits = 0;
   }
-  formSchema.hostingCredits = {
-    "type": "number",
-    "label": "hosting credits",
-    "disabled": true,
-    "default": user.hostingCredits
+
+  if (user.hostingCredits > 0) {
+    formSchema.hostingCredits = {
+      "type": "number",
+      "label": "hosting credits",
+      "disabled": true,
+      "default": user.hostingCredits
+    }
   }
 
   forms.generate({
