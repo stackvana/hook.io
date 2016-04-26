f384830e50398f75e0cd18b99f9e165faa3c1c00
Marak <marak.squires@gmail.com>
2016-04-26 04:02:40 -0400

    [view] White-label docs page based on hostname

--- view/docs.js
+++ view/docs.js
@@ -3,6 +3,10 @@ module['exports'] = function doc (opts, callback) {
       req = opts.request;
   var i18n = require('./helpers/i18n');
   i18n(req.i18n, $);
+  var out = $.html();
+  // var appName = "hook.io";
+  var appName = req.hostname;
+  out = out.replace(/\{\{appName\}\}/g, appName);
   //req.i18n.setLocale('de');
-  return callback(null, $.html());
+  return callback(null, out);
 };
