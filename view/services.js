8522fd3659bb9908304ce9d62d0e246833caf74d
Marak <marak.squires@gmail.com>
2016-05-07 13:14:57 -0400

    [view] Replace `hooks` view with `services`

--- view/services.js
+++ view/services.js
@@ -10,15 +10,17 @@ module['exports'] = function view (opts, callback) {
 
   if (typeof params.signedMeUp !== "undefined" || typeof params.s !== "undefined") {
     req.session.referredBy = req.params.owner;
-    return res.redirect("/");
+    //return res.redirect("/");
   }
 
-  if (!opts.request.isAuthenticated()) { 
-    return res.redirect('/login');
+  var loggedIn = false;
+  if (!opts.request.isAuthenticated()) {
+    loggedIn = true;
+    //return res.redirect('/login');
     //$('.navBar').remove()
   }
 
-  hook.find({owner: req.session.user }, function (err, hooks){
+  hook.find({owner: req.params.owner }, function (err, hooks){
     if (err) {
       return res.end(err.message);
     }
@@ -27,7 +29,10 @@ module['exports'] = function view (opts, callback) {
     if (typeof req.session.referredBy === "undefined") {
       req.session.referredBy = req.params.owner;
     }
-
+    if (req.params.owner !== req.session.user) {
+      $('.navBar').remove();
+      $('.servicesHeader').html(req.params.owner);
+    }
     if (hooks.length > 0) {
       // sort hooks alphabetically by name
       hooks = hooks.sort(function(a,b){
@@ -49,9 +54,13 @@ module['exports'] = function view (opts, callback) {
         var hookLink = "/" + hooks[h].owner + "/" + hooks[h].name + "";
         var priv = "";
         if (hooks[h].isPrivate) {
-          priv = "Private ";
+          priv = '<span title="Private Service / Restricted Access" class="octicon octicon-lock"></span> ';
+          if (req.params.owner === req.session.user) {
+            $('.hooks').append('<tr><td class="col-md-8">' + priv + '<a title="Hook Admin" href="' + hookLink + '/admin">' + hooks[h].name + '</a></td><td class="col-md-1" align="left"><a title="Run Hook" href="' + hookLink + '"><span class="mega-octicon octicon-triangle-right"></span></a></td><td class="col-md-1" align="left"><a title="View Source" href="' + hookLink + '/source"><span class="mega-octicon octicon-file-code"></span></a></td><td class="col-md-1" align="left"><a title="View Logs" href="' + hookLink + '/logs"><span class="mega-octicon octicon-list-ordered"></span></a></td><td class="col-md-1" align="left"><a title="Delete Hook" class="deleteLink" data-name="' + hooks[h].owner + "/" + hooks[h].name +'" href="' + hookLink + '/delete"><span class="mega-octicon octicon-trashcan"></span></a></td></tr>')
+          }
+        } else {
+          $('.hooks').append('<tr><td class="col-md-8">' + priv + '<a title="Hook Admin" href="' + hookLink + '/admin">' + hooks[h].name + '</a></td><td class="col-md-1" align="left"><a title="Run Hook" href="' + hookLink + '"><span class="mega-octicon octicon-triangle-right"></span></a></td><td class="col-md-1" align="left"><a title="View Source" href="' + hookLink + '/source"><span class="mega-octicon octicon-file-code"></span></a></td><td class="col-md-1" align="left"><a title="View Logs" href="' + hookLink + '/logs"><span class="mega-octicon octicon-list-ordered"></span></a></td><td class="col-md-1" align="left"><a title="Delete Hook" class="deleteLink" data-name="' + hooks[h].owner + "/" + hooks[h].name +'" href="' + hookLink + '/delete"><span class="mega-octicon octicon-trashcan"></span></a></td></tr>')
         }
-        $('.hooks').append('<tr><td class="col-md-8">' + priv + '<a title="Hook Admin" href="' + hookLink + '/admin">' + hooks[h].name + '</a></td><td class="col-md-1" align="left"><a title="Run Hook" href="' + hookLink + '"><span class="mega-octicon octicon-triangle-right"></span></a></td><td class="col-md-1" align="left"><a title="View Source" href="' + hookLink + '/source"><span class="mega-octicon octicon-file-code"></span></a></td><td class="col-md-1" align="left"><a title="View Logs" href="' + hookLink + '/logs"><span class="mega-octicon octicon-list-ordered"></span></a></td><td class="col-md-1" align="left"><a title="Delete Hook" class="deleteLink" data-name="' + hooks[h].owner + "/" + hooks[h].name +'" href="' + hookLink + '/delete"><span class="mega-octicon octicon-trashcan"></span></a></td></tr>')
       }
 
 
