8799b113cae415ad660b85d90b87944b83d6eb0f
Marak <marak.squires@gmail.com>
2016-05-07 12:44:28 -0400

    [view] Update pricing page

--- view/pricing.js
+++ view/pricing.js
@@ -3,6 +3,8 @@ var config = require("../config");
 module['exports'] = function view (opts, callback) {
   var $ = this.$, req = opts.req;
   var appName = req.hostname;
+  // if paid account
+  // $('.freeAccount').remove();
   var out = $.html();
   out = out.replace('{{stripePK}}', config.stripe.publicKey);
   out = out.replace(/\{\{appName\}\}/g, appName);
