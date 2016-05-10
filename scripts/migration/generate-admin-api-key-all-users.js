de8f75d64f5a6b9fdb0bfd3966fbf7b4fd28eae1
Marak <marak.squires@gmail.com>
2016-05-10 03:22:35 -0400

    [scripts] Migration script for generating keys

--- scripts/migration/generate-admin-api-key-all-users.js
+++ scripts/migration/generate-admin-api-key-all-users.js
@@ -1,39 +1,69 @@
 var config = require('../../config');
+var cache = require('../../lib/resources/cache');
 
 var user = require('../../lib/resources/user');
 var role = require('../../lib/resources/role');
-
 var keys = require('../../lib/resources/keys');
-
+var billing = require('../../lib/resources/billing');
+var async = require('async');
 var colors = require('colors');
-
 user.persist(config.couch);
 keys.persist(config.couch);
 
-user.all(function(err, results){
-  if(err) {
+
+// TODO
+user.all(function (err, results) {
+
+  if (err) {
     throw err;
   }
-  function setName () {
-    if (results.length === 0) {
-      process.exit();
-    }
-    
-    var result = results.pop();
-    result.name = result.name.toLowerCase();
+  console.log('loading user', results.length)
+  async.eachLimit(results, 2, createKey, function(err, re){
+    console.log('finished', err, re);
+    process.exit();
+  })
+  var updatedUsers = [];
+  var updatedKeys = [];
+  function createKey (_user, cb) {
+    _user.name = _user.name.toLowerCase();
     //result.githubOauth = true;
-    console.log(result.name.toLowerCase())
-    keys.create({ name: 'admin-access-key', owner: result.name, roles: Object.keys(role.roles) }, function(err, key){
+    console.log(_user.name);
+    keys.findOne({ name: "api-access-key", owner: _user.name }, function (err, k){
+      console.log(err)
       if (err) {
-        console.log(err.message, result.name)
-        return setName();
+        // create a new key
+        keys.create({ 
+          name: 'api-access-key',
+          owner: _user.name,
+          key_type: "internal",
+          roles: Object.keys(role.roles),
+          readOnly: true
+        }, function(err, key){
+          if (err) {
+            console.log(err.message, _user.name)
+            return cb();
+          } else {
+            console.log('created key', key.id);
+            user.findOne({name: _user.name }, function(err, u){
+              if (err) {
+                return cb();
+              }
+              u.hookAccessKey = key.hook_private_key;
+              u.save(function(){
+                cb();
+              });
+              
+            });
+          }
+        }); 
+      } else {
+        // do nothing, assume has has it
+        cb();
       }
-      console.log('created key', key)
-      setName();
-      
-    })
+
+    });
+
   }
-  setName();
 });
 
 console.log(role.roles);
