1b68e1d805243be838ad953c8b4a87578d8551b7
Marak <marak.squires@gmail.com>
2016-09-20 15:53:37 -0400

    [api] [view] Better support for removing env vars   * Now using JSON API and Ajax Save   * Fixes issue #258

--- view/env.js
+++ view/env.js
@@ -1,37 +1,19 @@
-// TODO: refactor whole page to accept standard object hash of values instead of key / value pair array...
-
 var user = require('../lib/resources/user');
 var cache = require("../lib/resources/cache");
 var resource = require('resource');
 var checkRoleAccess = require('../lib/server/routeHandlers/checkRoleAccess');
 var config = require('../config');
-
-// TODO: refactor env to be update friendly
-// will need to have "undefined" or "null" determine a value should be removed from document
-
 var psr = require('parse-service-request');
 
-var bodyParser = require('body-parser');
 module['exports'] = function view (opts, callback) {
   var req = opts.request, res = opts.response;
   var $ = this.$;
-  
-  // TODO: make so that env page can view without login to show autodocs
-  /*
-  if (!req.isAuthenticated()) { 
-    req.session.redirectTo = "/env";
-    return res.redirect('/login');
-  }
-  */
+
   var _user;
-  //bodyParser(req, res, fun)
-  bodyParser()(req, res, function bodyParsed(){
-  
-  psr(req, res, function (req, res){
+  psr(req, res, function (req, res, fields){
     var params = req.resource.params;
-    //console.log('params', params)
+
     checkRoleAccess({ req: req, res: res, role: "env::read" }, function (err, hasPermission) {
-      // console.log('check for role access', err, hasPermission)
       if (!hasPermission || req.resource.owner === "anonymous") { // don't allow anonymous hook update
         if (req.jsonResponse !== true && typeof params.hook_private_key === "undefined") {
           req.session.redirectTo = "/env";
@@ -45,13 +27,11 @@ module['exports'] = function view (opts, callback) {
     });
 
     function next () {
-      // console.log('trying to find', _user);
       user.findOne({ name: _user }, function(err, _user) {
         // console.log('found old user', params)
         if (err) {
           return callback(null, err.message);
         }
-
         var _env = _user.env || {}; // default to empty object if no env exists
         if (params.update || req.method === "POST") {
           // update is destructive and complete
@@ -59,7 +39,6 @@ module['exports'] = function view (opts, callback) {
           // all old fields are deleted and replaced with new values
           // TODO: move to resource.before hooks
           checkRoleAccess({ req: req, res: res, role: "env::write" }, function (err, hasPermission) {
-            // console.log('env check for role access', err, hasPermission);
             if (!hasPermission || req.resource.owner === "anonymous") { // don't allow anonymous hook update
               if (req.jsonResponse !== true && typeof params.hook_private_key === "undefined") {
                 req.session.redirectTo = "/env";
@@ -73,12 +52,11 @@ module['exports'] = function view (opts, callback) {
           });
 
           function _updateEnv () {
-            // console.log('_updateEnv', params)
             _user.env = {};
             // new JSON based API with env property containing object hash
             if (params.env) {
               for (var k in params.env) {
-                if (params.env[k] === null) {
+                if (params.env[k] === null || params.env[k] === "null") {
                   delete _env[k];
                 } else {
                   _env[k] = params.env[k];
@@ -94,9 +72,8 @@ module['exports'] = function view (opts, callback) {
               if (typeof params.value === "string") {
                 params.value = [params.value];
               }
-              //console.log('values', params)
-              //console.log('OLD USER', _user)
-              params.key.forEach(function(k, i){
+
+              params.key.forEach(function (k, i) {
                 if (k.length) {
                   // try to JSON.parse since value might be array or object
                   var val = params.value[i];
@@ -106,14 +83,13 @@ module['exports'] = function view (opts, callback) {
                     val = params.value[i];
                   }
                   // TODO: fix merging from /env form, delete undefined keys..s.
-                  if (val === null && typeof val !== "undefined") {
+                  if (val === null || typeof val === "undefined") {
                     delete _env[k];
                   } else {
                     _env[k] = val;
                   }
                 }
               });
-              // console.log('about to save', _env )
             }
 
             _user.env = _env;
@@ -131,7 +107,7 @@ module['exports'] = function view (opts, callback) {
                   url: req.url
                 });
                 if (req.jsonResponse) {
-                  return res.json(_user.env);
+                  return res.json({ status: 'updated'});
                 } else {
                   showEnv(true);
                 }
@@ -167,8 +143,8 @@ module['exports'] = function view (opts, callback) {
               v = JSON.stringify(v);
             }
             var _delete = '<a href="#remove" class="removeKey">X</a>';
-            var _key = '<input name="key" type="text" value="' + k + '" size="20"/>';
-            var _value = '<input name="value" type="text" value="' + quoteattr(v) + '" size="60"/>';
+            var _key = '<input name="key" class="key" type="text" value="' + k + '" size="20"/>';
+            var _value = '<input name="value" class="value" type="text" value="' + quoteattr(v) + '" size="60"/>';
             $('.env').append('<tr><td>' + _delete + '</td><td>' + _key + '</td><td>' + _value + '</td></tr>');
           };
           if (Object.keys(env).length === 0) {
@@ -177,7 +153,6 @@ module['exports'] = function view (opts, callback) {
           Object.keys(env).forEach(function(key){
             addRow(key, env[key]);
           });
-
           resource.emit('env::read', {
             ip: req.connection.remoteAddress,
             owner: req.session.user,
@@ -192,43 +167,5 @@ module['exports'] = function view (opts, callback) {
       });
 
     }
-
   });
-});
-  
 };
-
-
-
-//
-// Middleware for merging all querystring / request.body and route parameters,
-// into a common scope bound to req.resource.params
-//
-function mergeParams (req, res, next) {
-
-  req.resource = req.resource || {};
-  req.resource.params = {};
-  req.body = req.body || {};
-
-  //
-  // Iterate through all the querystring and request.body values and
-  // merge them into a single "data" argument
-  //
-  if (typeof req.params === 'object') {
-    Object.keys(req.params).forEach(function (p) {
-      req.resource.params[p] = req.param(p);
-    });
-  }
-
-  if (typeof req.query === 'object') {
-    Object.keys(req.query).forEach(function (p) {
-      req.resource.params[p] = req.query[p];
-    });
-  }
-
-  Object.keys(req.body).forEach(function (p) {
-    req.resource.params[p] = req.body[p];
-  });
-
-  next();
-}
