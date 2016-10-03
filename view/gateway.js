ea32471e02b295edb7521448a5405388a3750344
Marak <marak.squires@gmail.com>
2016-10-02 20:42:12 -0400

    [api] [refactor] Use `stack.plugins.rateLimiter`   * Replaces old rate limiting code with stack   * Also now using `stack.plugins.bodyParser`     * Required for `web` based validation     * Should work for all services

--- view/gateway.js
+++ view/gateway.js
@@ -2,6 +2,7 @@ var hook = require('../lib/resources/hook');
 var metric = require('../lib/resources/metric');
 var config = require('../config');
 var checkRoleAccess = require('../lib/server/routeHandlers/checkRoleAccess')
+var stack = require('stackvana');
 
 module['exports'] = function view (opts, callback) {
   var $ = this.$,
@@ -12,11 +13,6 @@ module['exports'] = function view (opts, callback) {
   var pool = config.workers;
   var remoteHandler = hook.runRemote({ pool: pool });
 
-  // TODO: pull value from config / paidPlans.js
-  /*
-  var config.MAX_SERVICE_CONCURRENCY = 2;
-  var config.MAX_SERVICE_EXECUTIONS_PER_CYCLE = 1000;
-  */
   // TODO: determine owner by API key or session
 
   // Remark: is all session code already being handled?
@@ -40,41 +36,26 @@ module['exports'] = function view (opts, callback) {
   function runService () {
     // line will be removed when role checks are working
     req.params.owner = req.params.owner || "anonymous";
-    // Get total amount of hits the user has for this billing period
-    metric.get("/" + req.params.owner + "/hits", function(err, total){
-
-      // if total hits for user account is exceeded, rate-limit
-      console.log('metric.' + req.params.owner + '.hits'.green, err, total);
-      if (Number(total) >= config.MAX_SERVICE_EXECUTIONS_PER_CYCLE) {
-        // TODO: better error message
-        return res.end('max executions per cycle hit, upgrade plan, wait, or request more');
-      }
-
-      // TODO: get req.user.plan to determine actual limits ( versus paid / non-paid limits )
-      // TODO: reset limits on the start of every month, or at the start of the billing cycle?
-      // Get total amount of running hooks for current user
-      metric.get("/" + req.params.owner + "/running", function (err, total) {
-        // if total running is greater than account concurrency limit, rate-limit the request
-        if (err) {
-          return res.end(err.message);
-        }
-        console.log('total'.green, total, config.MAX_SERVICE_CONCURRENCY)
-        if (Number(total) >= config.MAX_SERVICE_CONCURRENCY) {
-          // TODO: better error message
-          return res.end('max concurrency hit, try again shortly.');
-        }
-        console.log('metric.' + req.params.owner + '.running'.green, err, total);
-        _runRemote();
-      });
+    // TODO: Get total amount of hits the user has for this billing period
+    // TODO: check total hits against plan limits,
+    // TODO: reset limits on the start of every month, or at the start of the billing cycle?
+    // TODO: pull value from config / paidPlans.js
+    // TODO: add back specific gateway hits
+      // metric.incr("/" + service.owner + "/" + "gateway" + '/hits');
+    // WARNING: currently using default limit values ( see above note )
+    stack.plugins.rateLimiter({
+      provider: metric
+    })(req, res, function () {
+      _runRemote();
     });
   }
 
+  // run hook on remote worker
   function _runRemote () {
-    // run hook on remote worker
-    // console.log('calling remote handler', req.url);
+    // console.log('calling remote handler', req.url); // TODO: not full url
     return remoteHandler(req, res, function (err, r) {
-      console.log('remote handler ended?'.green)
-      console.log(err, r);
+      //console.log('remote handler ended?'.green)
+      //console.log(err, r);
       // TODO: check if callback makes it here everytime...
       // do nothing with the result
       // if the hook has been properly formatted, it should be able to call res.write res.end on it's own
