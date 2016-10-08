9bf1337d16f8b39a9fd58bc3e7350f431365091d
Marak <marak.squires@gmail.com>
2016-10-08 19:59:40 -0400

    [view] Adds basic `/top` section for new metrics

---
+++ view/top.js
@@ -0,0 +1,19 @@
+var metric = require('../lib/resources/metric');
+// TODO: add top running services / top hits report using sets and http://redis.io/commands/zrevrangebyscore methods
+module['exports'] = function topPresenter (opts, callback) {
+  var req = opts.req, res = opts.res, $ = this.$;
+  var appName = req.hostname;
+  metric.top('running', function(err, members){
+    if (err) {
+      return res.end(err.message);
+    }
+    $('.running').html(JSON.stringify(members, true , 2));
+    metric.top('hits', function(err, members){
+      if (err) {
+        return res.end(err.message);
+      }
+      $('.hits').html(JSON.stringify(members, true , 2));
+      callback(null, $.html());
+    });
+  });
+};
