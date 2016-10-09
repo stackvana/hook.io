10dee01f4f038be941b2e9e6c3aff9f90458c831
Marak <marak.squires@gmail.com>
2016-10-09 17:58:25 -0400

    [view] [fix] Add role checks to `/_src` endpoint   * Do not allow viewing of source for private

--- view/hook/_src.js
+++ view/hook/_src.js
@@ -1,4 +1,6 @@
 var hook = require('../../lib/resources/hook');
+var checkRoleAccess = require('../../lib/server/routeHandlers/checkRoleAccess');
+var config = require('../../config');
 
 module['exports'] = function view (opts, callback) {
   var req = opts.req, res = opts.res, $ = this.$;
@@ -16,83 +18,99 @@ module['exports'] = function view (opts, callback) {
     }
 
     var h = result[0];
-    
-    // note: we could be using the new pkg format
-    var extentions = {
-      ".js": "javascript",
-      ".coffee": "coffee-script",
-      ".lua": "lua",
-      ".php": "php",
-      ".pl": "perl",
-      ".py": "python", // Remark: You can also use the "--language python" option
-      ".py3": "python3", // Remark: You can also use the "--language python3" option
-      ".sh": "bash",
-      ".rb": "ruby",
-      ".tcl": "tcl",
-      ".ss": "scheme",
-      ".st": "smalltalk"
-    };
-    var file = "index";
-    Object.keys(extentions).forEach(function(ext){
-      if (extentions[ext] === h.language) {
-        file += ext;
+
+    checkRoleAccess({ req: req, res: res, role: "hook::source::read" }, function (err, hasPermission) {
+
+      // only protect source of private services
+      if (h.isPrivate !== true) {
+        hasPermission = true;
       }
+
+      if (!hasPermission) {
+        return res.end(config.messages.unauthorizedRoleAccess(req, "hook::source::read"));
+      }
+      next();
     });
 
-    $('.service').html(file);
+    function next () {
 
-    if (typeof h.themeSource === "undefined" || h.themeSource.length === 0) {
-      $('.view').parent().remove();
+        // note: we could be using the new pkg format
+        var extentions = {
+          ".js": "javascript",
+          ".coffee": "coffee-script",
+          ".lua": "lua",
+          ".php": "php",
+          ".pl": "perl",
+          ".py": "python", // Remark: You can also use the "--language python" option
+          ".py3": "python3", // Remark: You can also use the "--language python3" option
+          ".sh": "bash",
+          ".rb": "ruby",
+          ".tcl": "tcl",
+          ".ss": "scheme",
+          ".st": "smalltalk"
+        };
+        var file = "index";
+        Object.keys(extentions).forEach(function(ext){
+          if (extentions[ext] === h.language) {
+            file += ext;
+          }
+        });
+
+        $('.service').html(file);
+
+        if (typeof h.themeSource === "undefined" || h.themeSource.length === 0) {
+          $('.view').parent().remove();
+        }
+
+        if (typeof h.presenterSource === "undefined" || h.presenterSource.length === 0) {
+          $('.presenter').parent().remove();
+        }
+
+        if (typeof h.mschema === "undefined") {
+          $('.schema').parent().remove();
+        }
+
+        /*
+        h.themeSource
+        h.presenterSource
+        h.mschema
+        $('.service').html();
+        $('.view').html();
+        $('.presenter').html();
+        $('.schema').html();
+        */
+
+
+        req.hook = h;
+
+        switch (params.f) {
+
+          case 'view.html':
+            $('#code').text(h.themeSource);
+          break;
+
+          case 'presenter.js':
+            $('#code').text(h.presenterSource);
+          break;
+
+          case 'schema.js':
+            $('#code').text(JSON.stringify(h.mschema, true, 2));
+          break;
+
+          default:
+            $('#code').text(h.source);
+          break;
+
+        }
+
+        var out = $.html();
+        out = out.replace(/\{\{appName\}\}/g, appName);
+        callback(null, out);
+
     }
 
-    if (typeof h.presenterSource === "undefined" || h.presenterSource.length === 0) {
-      $('.presenter').parent().remove();
-    }
+  });
 
-    if (typeof h.mschema === "undefined") {
-      $('.schema').parent().remove();
-    }
-
-    /*
-    h.themeSource
-    h.presenterSource
-    h.mschema
-    $('.service').html();
-    $('.view').html();
-    $('.presenter').html();
-    $('.schema').html();
-    */
-    
-    
-    req.hook = h;
-
-    switch (params.f) {
-      
-      case 'view.html':
-        $('#code').text(h.themeSource);
-      break;
-
-      case 'presenter.js':
-        $('#code').text(h.presenterSource);
-      break;
-
-      case 'schema.js':
-        $('#code').text(JSON.stringify(h.mschema, true, 2));
-      break;
-      
-      default:
-        $('#code').text(h.source);
-      break;
-      
-    }
-    
-    var out = $.html();
-    out = out.replace(/\{\{appName\}\}/g, appName);
-    callback(null, out);
-
-  });  
-  
-  
 };
 
 module['exports'].useParentLayout = false;
