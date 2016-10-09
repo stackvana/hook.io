edd914e5e4d78b2f8ddc68f935ae97bbb542ddf2
Marak <marak.squires@gmail.com>
2016-10-09 13:09:43 -0400

    [dist] Update start scripts to new APIs   * Now clearing running count on start   * Using new directory structure for services

--- scripts/start-production.sh
+++ scripts/start-production.sh
@@ -1,12 +1,21 @@
+# clear all metrics for running hooks
+# this is needed in-case hook.io was shut down abruptly
+# will reset all running services concurrency count back to 0
+
+redis-cli del 'metrics/running' &
 # start main front-end server
-sudo mon -d "sudo node bin/server" --log server-log.txt &&
+sudo mon -d "sudo node bin/services/load-balancer " --log server-log.txt &&
+
+# start three front-end web servers
+sudo mon -d "sudo node bin/services/web" --log worker-logs.txt &&
+sudo mon -d "sudo node bin/services/web" --log worker-logs.txt &&
 
 # start five workers to run hooks
-sudo mon -d "sudo node bin/worker" --log worker-logs.txt &&
-sudo mon -d "sudo node bin/worker" --log worker-logs.txt &&
-sudo mon -d "sudo node bin/worker" --log worker-logs.txt &&
-sudo mon -d "sudo node bin/worker" --log worker-logs.txt &&
-sudo mon -d "sudo node bin/worker" --log worker-logs.txt &&
+sudo mon -d "sudo node bin/services/worker" --log worker-logs.txt &&
+sudo mon -d "sudo node bin/services/worker" --log worker-logs.txt &&
+sudo mon -d "sudo node bin/services/worker" --log worker-logs.txt &&
+sudo mon -d "sudo node bin/services/worker" --log worker-logs.txt &&
+sudo mon -d "sudo node bin/services/worker" --log worker-logs.txt &&
 
 # start Hook Packager Manager server
 # this managers hook package dependecy installations
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,10 @@
 #trap 'killall node' EXIT
 #trap 'killall node' SIGINT
 
+# clear all metrics for running hooks
+# this is needed in-case hook.io was shut down abruptly
+# will reset all running services concurrency count back to 0
+redis-cli del 'metrics/running' &
 
 # start loadbalancer1
 node ./bin/services/load-balancer &
@@ -10,19 +14,14 @@ node ./bin/services/web &
 node ./bin/services/web &
 node ./bin/services/web &
 
-# start websocket server
-# node ./bin/websocket-server &
-
 # start five workers to run hooks
-node ./bin/worker &
-node ./bin/worker &
-node ./bin/worker &
-node ./bin/worker &
-node ./bin/worker 
+node ./bin/services/worker &
+node ./bin/services/worker &
+node ./bin/services/worker &
+node ./bin/services/worker &
+node ./bin/services/worker
 
 # &node ./bin/hpm-server
-
-
 
 # start cron jobs
 # sudo node bin/cron
