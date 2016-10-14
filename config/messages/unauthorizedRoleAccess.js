c7bad574eaf6ca7fc0a762c532a35f63b123c521
Marak <marak.squires@gmail.com>
2016-10-13 23:04:28 -0400

    [api] Better unauthorized role access messages   * Adds keyName to error if available

--- config/messages/unauthorizedRoleAccess.js
+++ config/messages/unauthorizedRoleAccess.js
@@ -24,15 +24,18 @@ module['exports'] = function unauthorizedRoleAccess (req, role) {
     //str += "Try again with a diffirent `hook_private_key` value?\n\n";
   }
 
-  str += ('"' + req.session.user + '" does not have the role "' + role + '" which is required to access "' + req._parsedUrl.pathname + '"');
+  if (typeof req.resource.keyName !== "undefined") {
+    str += ('"' + req.resource.keyName  + '" does not have the role "' + role + '" which is required to access "' + req._parsedUrl.pathname + '"');
+  } else {
+    str += ('"' + req.session.user  + '" does not have the role "' + role + '" which is required to access "' + req._parsedUrl.pathname + '"');
+  }
 
   if (req.session.user === "anonymous") {
     str += "\n\nIf you are the owner of this resource try logging in at https://" + config.app.domain + "/login";
   }
 
   if (typeof req.resource.params.hook_private_key !== "undefined") {
-    // if key was provided but the role check failed ( since it reached here ) show specific error message
-    //    str += "\n\nA role access check was attempted but failed with key: " + '"' + req.resource.params.hook_private_key + '"';
+    // do nothing
   } else {
     // provide instructions to provide keys
     str += "\n\nIf any access keys have been created you can also provide a `hook_private_key` parameter to access the service.";
