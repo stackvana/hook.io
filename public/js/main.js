f87bf53bba91454ed665cc622974736dd0724b28
Marak <marak.squires@gmail.com>
2016-12-17 22:38:57 -0500

    [view] [fix] Disable errant js handler

--- public/js/main.js
+++ public/js/main.js
@@ -120,8 +120,10 @@
 		$( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
 	   }
 	  });
+	  /*
 	  $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
 	   " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+	   */
 
 	/*---------------------
 	 collapse 
