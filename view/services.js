54e1531bbe2282e4a0403433ddc903cda2793c2a
Marak <marak.squires@gmail.com>
2016-12-21 20:10:01 -0500

    [view] [fix] Users can view other users hooks   * Relaxed role check (default public services)   * Does filter out private hooks   * Will not show private hooks to non-owners

--- view/services.js
+++ view/services.js
@@ -29,8 +29,10 @@ module['exports'] = function view (opts, callback) {
     for (var p in fields) {
       params[p] = fields[p];
     }
+    return finish();
+    /* bypass role check for now, anyone can view other users non-private hooks ( filter is performed below )
     checkRoleAccess({ req: req, res: res, role: "hook::find" }, function (err, hasPermission) {
-      if (!hasPermission /* || req.resource.owner === "anonymous" */ ) { // don't allow anonymous hook listing?
+      if (!hasPermission) { // don't allow anonymous hook listing?
         if (req.jsonResponse !== true && typeof params.hook_private_key === "undefined") {
           req.session.redirectTo = "/services";
           return res.redirect('/login');
@@ -40,6 +42,7 @@ module['exports'] = function view (opts, callback) {
         finish();
       }
     });
+    */
   });
 
   function finish () {
@@ -59,6 +62,17 @@ module['exports'] = function view (opts, callback) {
       if (err) {
         return res.end(err.message);
       }
+      // TODO: filter out private hooks if session owner doesn't match hook owner
+      // if current user is not owner, filter out private hooks
+       hooks = hooks.filter(function(item){
+        if (typeof req.session.user === "undefined" && item.isPrivate) {
+          return false
+        }
+        if (item.isPrivate && typeof req.session.user !== "undefined" && (item.owner !== req.session.user.toLowerCase())) {
+          return false;
+        }
+        return item;
+      });
 
       if (req.jsonResponse) {
         return res.json(hooks);
@@ -71,6 +85,7 @@ module['exports'] = function view (opts, callback) {
         //$('.navBar').remove();
         $('.servicesHeader').html(req.params.owner);
       }
+
       if (hooks.length > 0) {
         // sort hooks alphabetically by name
         hooks = hooks.sort(function(a,b){
@@ -79,29 +94,20 @@ module['exports'] = function view (opts, callback) {
           return 0;
         });
 
-        // if current user is not owner, filter out private hooks
-         hooks = hooks.filter(function(item){
-          if (item.isPrivate && item.owner !== req.session.user.toLowerCase()) {
-            return false;
-          }
-          return item;
-        });
-
-        for (var h in hooks) {
-          // TODO: add ability to delete hooks https://github.com/bigcompany/hook.io/issues/47
-          var hookLink = "/" + hooks[h].owner + "/" + hooks[h].name + "";
+        hooks.forEach(function(h){
+          var hookLink = "/" + h.owner + "/" + h.name + "";
           var priv = "";
-          if (hooks[h].isPrivate) {
+          if (h.isPrivate) {
             priv = '<span title="Private Service / Restricted Access" class="octicon octicon-lock"></span> ';
             if (req.params.owner === req.session.user || req.url === "/services") {
-              $('.hooks').append('<tr><td class="col-md-8">' + priv + '<a title="Hook Admin" href="' + hookLink + '/admin">' + hooks[h].name + '</a></td><td class="col-md-1" align="left"><a title="Run Hook" href="' + hookLink + '"><span class="mega-octicon octicon-triangle-right"></span></a></td><td class="col-md-1" align="left"><a title="View Source" href="' + hookLink + '/source"><span class="mega-octicon octicon-file-code"></span></a></td><td class="col-md-1" align="left"><a title="View Logs" href="' + hookLink + '/logs"><span class="mega-octicon octicon-list-ordered"></span></a></td><td class="col-md-1" align="left"><a title="Delete Hook" class="deleteLink" data-name="' + hooks[h].owner + "/" + hooks[h].name +'" href="' + hookLink + '/delete"><span class="mega-octicon octicon-trashcan"></span></a></td></tr>')
+              $('.hooks').append('<tr><td class="col-md-8">' + priv + '<a title="Hook Admin" href="' + hookLink + '/admin">' + h.name + '</a></td><td class="col-md-1" align="left"><a title="Run Hook" href="' + hookLink + '"><span class="mega-octicon octicon-triangle-right"></span></a></td><td class="col-md-1" align="left"><a title="View Source" href="' + hookLink + '/source"><span class="mega-octicon octicon-file-code"></span></a></td><td class="col-md-1" align="left"><a title="View Logs" href="' + hookLink + '/logs"><span class="mega-octicon octicon-list-ordered"></span></a></td><td class="col-md-1" align="left"><a title="Delete Hook" class="deleteLink" data-name="' + h.owner + "/" + h.name +'" href="' + hookLink + '/delete"><span class="mega-octicon octicon-trashcan"></span></a></td></tr>')
             }
           } else {
-            $('.hooks').append('<tr><td class="col-md-8">' + priv + '<a title="Hook Admin" href="' + hookLink + '/admin">' + hooks[h].name + '</a></td><td class="col-md-1" align="left"><a title="Run Hook" href="' + hookLink + '"><span class="mega-octicon octicon-triangle-right"></span></a></td><td class="col-md-1" align="left"><a title="View Source" href="' + hookLink + '/source"><span class="mega-octicon octicon-file-code"></span></a></td><td class="col-md-1" align="left"><a title="View Logs" href="' + hookLink + '/logs"><span class="mega-octicon octicon-list-ordered"></span></a></td><td class="col-md-1" align="left"><a title="Delete Hook" class="deleteLink" data-name="' + hooks[h].owner + "/" + hooks[h].name +'" href="' + hookLink + '/delete"><span class="mega-octicon octicon-trashcan"></span></a></td></tr>')
+            $('.hooks').append('<tr><td class="col-md-8">' + priv + '<a title="Hook Admin" href="' + hookLink + '/admin">' + h.name + '</a></td><td class="col-md-1" align="left"><a title="Run Hook" href="' + hookLink + '"><span class="mega-octicon octicon-triangle-right"></span></a></td><td class="col-md-1" align="left"><a title="View Source" href="' + hookLink + '/source"><span class="mega-octicon octicon-file-code"></span></a></td><td class="col-md-1" align="left"><a title="View Logs" href="' + hookLink + '/logs"><span class="mega-octicon octicon-list-ordered"></span></a></td><td class="col-md-1" align="left"><a title="Delete Hook" class="deleteLink" data-name="' + h.owner + "/" + h.name +'" href="' + hookLink + '/delete"><span class="mega-octicon octicon-trashcan"></span></a></td></tr>')
           }
-        }
+        });
 
-        if (Object.keys(hooks).length > 0) {
+        if (hooks.length > 0) {
           $('.noHooks').remove();
         } else {
           $('.hooks').remove();
