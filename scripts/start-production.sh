dc29cf139dcb06e6cbe80c4216bfcf22cf441a4b
Marak <marak.squires@gmail.com>
2016-12-23 16:08:53 -0500

    [bin] [refactor] Make services proper binaries

--- scripts/start-production.sh
+++ scripts/start-production.sh
@@ -4,23 +4,23 @@
 
 redis-cli del 'metrics/running' &
 # start main front-end server
-sudo mon -d "sudo node bin/services/load-balancer " --log server-log.txt &&
+sudo mon -d "sudo ./bin/services/load-balancer " --log server-log.txt &&
 
 # start three front-end web servers
-sudo mon -d "sudo node bin/services/web" --log worker-logs.txt &&
-sudo mon -d "sudo node bin/services/web" --log worker-logs.txt &&
+sudo mon -d "sudo ./bin/services/web" --log worker-logs.txt &&
+sudo mon -d "sudo ./bin/services/web" --log worker-logs.txt &&
 
 # start five workers to run hooks
-sudo mon -d "sudo node bin/services/worker" --log worker-logs.txt &&
-sudo mon -d "sudo node bin/services/worker" --log worker-logs.txt &&
-sudo mon -d "sudo node bin/services/worker" --log worker-logs.txt &&
-sudo mon -d "sudo node bin/services/worker" --log worker-logs.txt &&
-sudo mon -d "sudo node bin/services/worker" --log worker-logs.txt &&
+sudo mon -d "sudo ./bin/services/worker" --log worker-logs.txt &&
+sudo mon -d "sudo ./bin/services/worker" --log worker-logs.txt &&
+sudo mon -d "sudo ./bin/services/worker" --log worker-logs.txt &&
+sudo mon -d "sudo ./bin/services/worker" --log worker-logs.txt &&
+sudo mon -d "sudo ./bin/services/worker" --log worker-logs.txt &&
 
 # start Hook Packager Manager server
 # this managers hook package dependecy installations
-sudo mon -d "sudo node bin/hpm-server" --log hpm-logs.txt &&
+sudo mon -d "sudo ./bin/hpm-server" --log hpm-logs.txt &&
 
 # start cron process
 # this will exit(0) every ~60 seconds and restart
-sudo mon -d "sudo node bin/cron" --log cron-logs.txt
+sudo mon -d "sudo ./bin/cron" --log cron-logs.txt
--- scripts/start.sh
+++ scripts/start.sh
@@ -4,24 +4,24 @@
 # clear all metrics for running hooks
 # this is needed in-case hook.io was shut down abruptly
 # will reset all running services concurrency count back to 0
-redis-cli del 'metrics/running' &
+redis-cli -a "password" del 'metrics/running' &
 
 # start loadbalancer1
-node ./bin/services/load-balancer &
+./bin/services/load-balancer &
 
 # start three front-end web servers
-node ./bin/services/web &
-node ./bin/services/web &
-node ./bin/services/web &
+./bin/services/web &
+./bin/services/web &
+./bin/services/web &
 
 # start five workers to run hooks
-node ./bin/services/worker &
-node ./bin/services/worker &
-node ./bin/services/worker &
-node ./bin/services/worker &
-node ./bin/services/worker
+./bin/services/worker &
+./bin/services/worker &
+./bin/services/worker &
+./bin/services/worker &
+./bin/services/worker
 
-# &node ./bin/hpm-server
+# &./bin/hpm-server
 
 # start cron jobs
-# sudo node bin/cron
+# sudo bin/cron
