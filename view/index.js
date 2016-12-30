681bb2830e0be0cd967dcebb0316e2c523a26274
Marak <marak.squires@gmail.com>
2016-12-29 19:47:38 -0500

    [view] [refactor] Remove legacy `User.find`   * Fixes issue with curl signup

--- view/index.js
+++ view/index.js
@@ -2,6 +2,7 @@ var config = require('../config');
 var hook = require('../lib/resources/hook');
 var hooks = require('microservice-examples');
 var slug = require('slug');
+
 slug.defaults.modes['rfc3986'] = {
     replacement: '-',      // replace spaces with replacement
     symbols: true,         // replace unicode symbols or not
@@ -12,13 +13,14 @@ slug.defaults.modes['rfc3986'] = {
 slug.charmap['@'] = "-";
 slug.charmap['.'] = "-";
 
-// TODO: this should be part of mschema. see: https://github.com/mschema/mschema/issues/10
-var address = {
-  "type": "string",
-  "regex": /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i,
-};
 
 module['exports'] = function view (opts, callback) {
+
+  // TODO: this should be part of mschema. see: https://github.com/mschema/mschema/issues/10
+  var address = {
+    "type": "string",
+    "regex": /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i,
+  };
 
   var userResource = require('../lib/resources/user');
 
@@ -71,7 +73,6 @@ module['exports'] = function view (opts, callback) {
   //
   // end curl signups
   //
-
   // Render a cURL friendly response
   if (req.headers.accept === "*/*") {
     var address = "";
@@ -182,31 +183,15 @@ module['exports'] = function view (opts, callback) {
   } else {
     user = user.toLowerCase();
     var query = { name: user };
-    return userResource.find(query, function(err, results){
-      if (err) {
-        return res.end(err.message);
-      }
-      if (results.length === 0) {
-        $('.userBar').remove();
-        var out = $.html();
-        out = out.replace('{{hook}}', JSON.stringify(boot, true, 2));
-        return callback(null, out);
-        // return res.end('Unable to find user. This error should never happen. Please contact hookmaster@hook.io immediately.');
-      } else {
-        // do nothing
-      }
-      var u = results[0];
-      $('.userBar .welcome').html(i.__('Welcome') + ' <strong>' + user + "</strong>!")
-      $('.loginBar').remove();
-      $('.featuresDiv').remove();
-      $('.hookStats').remove();
-      // $('.tagline').remove();
-      $('.yourHooks').attr("href", "/" + user);
-      // $('.splash').remove();
-      var out = $.html();
-      out = out.replace('{{hook}}', JSON.stringify(boot, true, 2));
-      return callback(null, out);
-    });
+    $('.userBar .welcome').html(i.__('Welcome') + ' <strong>' + user + "</strong>!")
+    $('.loginBar').remove();
+    $('.featuresDiv').remove();
+    $('.hookStats').remove();
+    // $('.tagline').remove();
+    $('.yourHooks').attr("href", "/" + user);
+    // $('.splash').remove();
+    var out = $.html();
+    out = out.replace('{{hook}}', JSON.stringify(boot, true, 2));
+    return callback(null, out);
   }
-
 };
