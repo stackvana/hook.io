8614ccb98718b2712fc55a55b57ee896cb48babc
Marak <marak.squires@gmail.com>
2017-01-02 13:55:59 -0500

    [blog] [fix] Should be HTML, not markdown

--- view/blog/the-monolith-versus-the-microservice-a-tale-of-two-applications.js
+++
@@ -1,9 +0,0 @@
-var df = require('dateformat');
-
-module['exports'] = function view (opts, callback) {
-  var self = this, $ = this.$;
-  callback(null, $.html());
-};
-
-// cache this page indefintely
-module['exports'].cache = 0;
