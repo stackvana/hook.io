363a86c73ace925a14a9b5b352c06e2256d3cab3
Marak <marak.squires@gmail.com>
2017-09-06 17:55:28 -0400

    [view] Better rendering of /top

--- view/top.js
+++ view/top.js
@@ -1,6 +1,20 @@
+var forms = require('mschema-forms');
+
+
 var metric = require('../lib/resources/metric');
 // TODO: add top running services / top hits report using sets and http://redis.io/commands/zrevrangebyscore methods
 module['exports'] = function topPresenter (opts, callback) {
+  var formSchema = {};
+  formSchema.user = {
+     "type": "string",
+     "default": "bob"
+   };
+
+   formSchema.hits = {
+      "type": "string",
+      "default": "42"
+    };
+
   var req = opts.req, res = opts.res, $ = this.$;
   var appName = req.hostname;
   if (req.session.user !== "marak") {
@@ -11,12 +25,59 @@ module['exports'] = function topPresenter (opts, callback) {
       return res.end(err.message);
     }
     $('.running').html(JSON.stringify(members, true , 2));
-    metric.top('hits', function(err, members){
-      if (err) {
-        return res.end(err.message);
-      }
-      $('.hits').html(JSON.stringify(members, true , 2));
-      callback(null, $.html());
+    // turn members into something useful
+    var _sorted = [];
+    
+    while (members.length > 0) {
+      var user = members.shift();
+      var hits = members.shift();
+      _sorted.push({ user: user, hits: hits});
+    }
+    
+    forms.generate({
+      type: "grid",
+      form: {
+        legend: 'top form',
+        submit: "Submit",
+        action: ""
+      },
+      data: _sorted,
+      schema: formSchema,
+      }, function (err, result){
+        $('.running').html(result);
+        
+        metric.top('hits', function(err, members){
+           if (err) {
+             return res.end(err.message);
+           }
+           //$('.hits').html(JSON.stringify(members, true , 2));
+           var _sorted = [];
+            while (members.length > 0) {
+               var user = members.shift();
+               var hits = members.shift();
+               _sorted.push({ user: user, hits: hits});
+             }
+
+             forms.generate({
+               type: "grid",
+               name: 'hits-forms',
+               form: {
+                 legend: 'top form',
+                 submit: "Submit",
+                 action: ""
+               },
+               data: _sorted,
+               schema: formSchema,
+               }, function (err, result){
+                 $('.hits').html(result);
+                 
+                 callback(null, $.html());
+
+             });
+         });
+        
     });
+    
+ 
   });
 };
