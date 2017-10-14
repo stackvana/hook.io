dc526e4ff4d935b19cca71fb6affb67f4ef5e724
Marak <marak.squires@gmail.com>
2017-10-14 18:30:45 -0400

    [test] Added all languages hello world tests   * Attempts to run all hello world examples   * Adds stubs for all languages echo world tests   * Ignores testing alpha languages   * Copied from microcule test suite

--- tests/client/hook-echo-tests.js
+++ tests/client/hook-echo-tests.js
@@ -3,6 +3,9 @@ var r = require('../lib/helpers/_request');
 var config = require('../config');
 var baseURL = config.baseUrl;
 var startDevCluster = require('../lib/helpers/startDevCluster');
+var examples = require('microcule-examples');
+var async = require('async');
+var pl = require('../../lib/resources/programmingLanguage');
 
 // hook-api-test.js
 var tap = require("tape");
@@ -14,16 +17,10 @@ var sdk = require('hook.io-sdk');
 
 var testUser = config.testUsers.bobby;
 
-var _config = {};
-_config.host = "localhost";
-_config.port = 9999;
-_config.protocol = 'http';
-_config.accessKey = testUser.admin_key;
-var client = sdk.createClient(_config);
+var client = sdk.createClient(testUser.hookSdk);
 
 tap.test('start the dev cluster', function (t) {
   startDevCluster({}, function (err, servers) {
-    console.log(servers)
     t.ok('cluster started');
     // should not require a timeout, probably issue with one of the services starting
     // this isn't a problem in production since these services are intended to start independant of each other
@@ -73,7 +70,6 @@ tap.test('post to the echo hook with JSON data', function (t) {
     }
   }, function (err, echo) {
     t.error(err, 'did not error');
-    console.log('eee', echo)
     t.equal(echo.baz, "boz", "echo'd back arbitrary json parameter");
     t.end();
   });
@@ -85,7 +81,7 @@ tap.test('post to the echo hook with JSON data', function (t) {
 
 */
 
-tap.test('submit a URL-encoded form to the echo hook with form data', function (t) {
+tap.test('submit a URL-encoded form to the javascript echo hook with form data', function (t) {
   r({ 
     uri: baseURL + "/examples/echo", 
     method: "post",
@@ -99,6 +95,122 @@ tap.test('submit a URL-encoded form to the echo hook with form data', function (
   });
 });
 
+tap.test('submit a URL-encoded form to the lua echo hook with form data', function (t) {
+  r({ 
+    uri: baseURL + "/examples/lua-echo", 
+    method: "post",
+    form: {
+      "baz": "boz"
+    }
+  }, function (err, echo) {
+    t.error(err, 'did not error');
+    t.equal(echo.baz, "boz", "echo'd back arbitrary form parameter");
+    t.end();
+  });
+});
+
+// TODO: search examples for all *-echo tests and call them in loop
+tap.test('attempt to call all possible *-hello-world examples', function (t) {
+
+  var customResponses = {
+    'r': '[1] "hello world"\n'
+  };
+
+  var helloExamples = [];
+  pl.alpha.push('babel')
+  Object.keys(examples.services).forEach(function(s){
+    if (s.search(/hello/) !== -1 && pl.alpha.indexOf(examples.services[s].language) === -1) {
+      helloExamples.push(s);
+    }
+  });
+
+  async.eachSeries(helloExamples, function iter (item, next) {
+    var lang = examples.services[item].language;
+    r({ 
+      uri: baseURL + "/examples/" + item, 
+      method: "GET"
+    }, function (err, body) {
+      var customResponses = {
+        "r": '[1] "hello world"\n',
+        "python-wsgi-hello-world": "hello world\n"
+      };
+
+      var noCarriageReturn = ["perl", "scheme", "php"];
+      if (typeof customResponses[lang] !== 'undefined') {
+        t.equal(body, customResponses[lang], 'got correct response from ' + lang);
+        next();
+        return;
+      }
+
+      if (typeof customResponses[item] !== 'undefined') {
+        t.equal(body, customResponses[item], 'got correct response from ' + item);
+        next();
+        return;
+      }
+
+      var doCRLF = [];
+      var crlf = (process.platform == 'win32')?'\r\n':'\n';
+      if (noCarriageReturn.indexOf(lang) !== -1) {
+        t.equal(body, 'hello world', 'got correct response from ' + lang);
+      } else if (doCRLF.indexOf(lang) !== -1) {
+        t.equal(body, 'hello world'+crlf, 'got correct response from ' + lang);
+      } else {
+        if (noCarriageReturn.indexOf(lang) !== -1) {
+          t.equal(body, 'hello world', 'got correct response from ' + lang);
+        } else {
+          t.equal(body, 'hello world\n', 'got correct response from ' + lang);
+        }
+      }
+      next();
+    });
+  }, function complete (err) {
+    t.end();
+  });
+
+});
+
+/*
+
+tap.test('attempt to call all possible *-echo examples', function (t) {
+//  console.log(Object.keys(examples.services))
+  
+  var echoExamples = [];
+
+  Object.keys(examples.services).forEach(function(s){
+    if (s.search(/echo/) !== -1) {
+      echoExamples.push(s);
+    }
+  });
+
+  async.eachSeries(echoExamples, function iter (item, next) {
+    r({ 
+      uri: baseURL + "/examples/" + item, 
+      method: "POST",
+      json: {
+        "baz": "boz"
+      }
+    }, function (err, echo) {
+      t.error(err, 'did not error');
+      // certain languages don't return JSON by default
+      switch (item) {
+        case 'bash-echo':
+          t.equal(echo, "baz=boz\n", "echo'd back correct value");
+        break;
+        case 'clisp-echo':
+          t.equal(echo, '\n#S(HASH-TABLE :TEST FASTHASH-EQL (BAZ . "boz")) \n', "echo'd back correct value");
+        break;
+        default:
+          t.equal(echo.baz, "boz", "echo'd back arbitrary json parameter");
+        break;
+      }
+      next();
+    });
+  }, function end (){
+    t.end();
+  });
+
+});
+*/
 /*
 
     TODO: move to seperate tests, SCHEMA TESTS, needs seperate service
@@ -121,13 +233,9 @@ tap.test('post the echo hook and check default schema data', function (t) {
   });
 });
 
-
-
 tap.test('perform hard shutdown of cluster', function (t) {
   t.end('cluster is shutting down');
   setTimeout(function(){
     process.exit();
   }, 10);
 });
-
-return;
