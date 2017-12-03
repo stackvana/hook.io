5b86d98bf6c5b03be78d6cff815eb53a713a5961
Marak <marak.squires@gmail.com>
2017-12-02 22:20:47 -0500

    [view] [ux] [refactor] Keys form and copy   * Updates copy for keys and roles   * Removes usage of `resource-forms` library   * Now using custom grid render for keys

--- view/keys/index.js
+++ view/keys/index.js
@@ -1,6 +1,5 @@
-var forms = require('resource-forms'),
-keys = require('../../lib/resources/keys'),
-mschemaForms = require('mschema-forms');
+var keys = require('../../lib/resources/keys'),
+forms = require('mschema-forms');
 
 var uuid = require('node-uuid');
 
@@ -29,6 +28,7 @@ module['exports'] = function view (opts, callback) {
 
  if (!req.isAuthenticated()) {
    $('.keys').remove();
+   $('.keysStatus').remove();
    // return callback(null, $.html());
  } else {
    $('.loginLink').remove();
@@ -58,154 +58,244 @@ module['exports'] = function view (opts, callback) {
        return callback(null, out);
        //return res.redirect('/login');
      } else {
-       $('.loginBar').remove();
-       finish();
+        $('.loginBar').remove();
+        finish();
      }
    }
  });
 
  function finish () {
+   var keySchema = {
+     name: {
+       type: 'string',
+       label: '<h3>Name</h3>',
+       placeholder: 'admin-access-key',
+       required: true,
+       minLength: 1,
+       maxLength: 50
+     },
+     /*
+     owner: {
+       type: 'string',
+       format: 'hidden',
+       required: true,
+       minLength: 1,
+       maxLength: 50,
+       default: req.session.user
+     },
+     */
+     /*
+     ,
+     hook_public_key: {
+       type: 'string',
+       label: "Public Key",
+       description: 'your public key',
+       required: true,
+       minLength: 1,
+       maxLength: 255,
+       size: 40,
+       disabled: true,
+       //            default: "public-" + new Date().getTime()
+       default: uuid()
+     },
+     */
+     hook_private_key: {
+       type: 'string',
+       label: "Private Key",
+       required: true,
+       minLength: 1,
+       maxLength: 255,
+       size: 40,
+       default: uuid(),
+       private: true
+     },
+     roles: {
+       label: "<h3>Roles</h3>",
+       description: "pick granular roles",
+       type: "string",
+       format: "checkbox",
+       // TODO: make actual array type instead of string
+       // type: "array",
+       customValue: true,
+       selectAll: true,
+       selectNone: true,
+       enum: role.roles,
+       defaultState: "checked",
+       required: true
+     },
+     customRoles: {
+       label: "<h3>Custom Roles</h3>",
+       hint: 'You can specify a custom string as a new role. This can be checked using a <a href="/roles#custom-role-checks">Custom Role Check</a>',
+       type: "string",
+       placeholder: 'foo::bar,hook::custom1,files::admin',
+       // TODO: make actual array type instead of string
+       // type: "array",
+       required: false,
+       default: ""
+     },
+     /*
+     key_type: {
+       type: 'string',
+       label: "Key Type",
+       enum: ['user', 'service'],
+       description: 'Your custom domain name. Example: marak.com',
+       required: true,
+       minLength: 1,
+       maxLength: 50
+     },
+     */
+   };
    bodyParser()(req, res, function bodyParsed() {
      mergeParams(req, res, function (){});
 
-     if (typeof req.resource.params.roles === "string") {
-       req.resource.params.roles = [req.resource.params.roles];
-     }
+    if (typeof req.resource.params.roles === "string") {
+      req.resource.params.roles = [req.resource.params.roles];
+    }
 
-     if (req.jsonResponse) {
-       if (req.method === "POST") {
-         var validate = mschema.validate(req.resource.params, self.schema);
-         if (!validate.valid) {
-           validate.status = "error";
-           return res.json(validate);
-         } else {
-           return keys.create(req.resource.params, function(err, result){
-             if (err) {
-               return res.end(err.message)
-             }
-             return res.json(result);
-           });
-         }
-       } else {
-         return res.json({ "status": "autodocs"});
-       } 
-     }
+    req.resource.params.roles = req.resource.params.roles || [];
+    if (typeof req.resource.params.customRoles === "string") {
+      // split by comma value for multiple roles
+      // TODO: trim any whitespace ?
+      var customRoles = req.resource.params.customRoles.split(',');
+      customRoles.forEach(function(customRole){
+        req.resource.params.roles.push(customRole);
+      });
+    }
 
-     req.resource.params.roles = req.resource.params.roles || [];
-     if (typeof req.resource.params.customRoles === "string") {
-       // split by comma value for multiple roles
-       // TODO: trim any whitespace ?
-       var customRoles = req.resource.params.customRoles.split(',');
-       customRoles.forEach(function(customRole){
-         req.resource.params.roles.push(customRole);
-       });
-     }
-     //console.log('merged params', req.resource.params)
-     var middle = forms.generate({
-        view: 'grid-with-form',
-        resource: keys,
-        action: '/keys',
-        params: req.resource.params,
-        req: req,
-        res: res,
-        query: { owner: req.session.user },
-        useLayout: false,
-        form: {
-          create: {
-            legend: 'Generate a new Key',
-            submit: "Add Key"
-          },
-          grid: {
-            legend: 'Your Keys',
-            keys: ['name', 'hook_private_key', 'roles']
-          }
-        },
-        schema: {
-          name: {
-            type: 'string',
-            label: '<h3>Name</h3>',
-            description: 'Your custom domain name. Example: marak.com',
-            placeholder: 'admin-access-key',
-            required: true,
-            minLength: 1,
-            maxLength: 50
-          },
-          owner: {
-            type: 'string',
-            format: 'hidden',
-            description: 'Your custom domain name. Example: marak.com',
-            required: true,
-            minLength: 1,
-            maxLength: 50,
-            default: req.session.user
-          },
-          /*
-          ,
-          hook_public_key: {
-            type: 'string',
-            label: "Public Key",
-            description: 'your public key',
-            required: true,
-            minLength: 1,
-            maxLength: 255,
-            size: 40,
-            disabled: true,
-            //            default: "public-" + new Date().getTime()
-            default: uuid()
-          },
-          */
-          hook_private_key: {
-            type: 'string',
-            label: "Private Key",
-            description: 'Your custom domain name. Example: marak.com',
-            required: true,
-            minLength: 1,
-            maxLength: 255,
-            size: 40,
-            default: uuid(),
-            private: true
-          },
-          customRoles: {
-            label: "<h3>Custom Roles</h3>",
-            type: "string",
-            placeholder: 'foo::bar,hook::custom1,files::admin',
-            // TODO: make actual array type instead of string
-            // type: "array",
-            required: false
-          },
-          roles: {
-            label: "<h3>Roles</h3>",
-            type: "string",
-            format: "checkbox",
-            // TODO: make actual array type instead of string
-            // type: "array",
-            customValue: true,
-            selectAll: true,
-            selectNone: true,
-            enum: role.roles,
-            defaultState: "checked",
-            required: true
-          }
-          /*
-          key_type: {
-            type: 'string',
-            label: "Key Type",
-            enum: ['user', 'service'],
-            description: 'Your custom domain name. Example: marak.com',
-            required: true,
-            minLength: 1,
-            maxLength: 50
-          },
-          */
-        }
-      }, function (err, result){
+    var params = req.resource.params;
+    if (typeof params.submitted !== 'undefined') {
+      // process posted form data
+      params.owner = req.session.user;
+      return keys.create(params, function (err, result) {
         if (err) {
           return res.end(err.message);
         }
-        $('.keys').html(result);
-        return callback(null, $.html());
-        });
+        $('.status').html('Created!');
+        showForms();
       });
+    } else {
+      if (params.destroy) {
+        // TODO: add role check
+        $('.status').html('Deleted!');
+        return keys.destroy(params.id, function (err) {
+          if (err) {
+            return res.end(err.message);
+          }
+          showForms();
+        });
+      } else {
+        showForms();
+      }
+    }
+
+     function showForms () {
+       if (req.jsonResponse) {
+         if (req.method === "POST") {
+           var validate = mschema.validate(req.resource.params, self.schema);
+           if (!validate.valid) {
+             validate.status = "error";
+             return res.json(validate);
+           } else {
+             return keys.create(req.resource.params, function(err, result){
+               if (err) {
+                 return res.end(err.message)
+               }
+               return res.json(result);
+             });
+           }
+         } else {
+           return res.json({ "status": "autodocs"});
+         }
+       }
+
+       //console.log('merged params', req.resource.params)
+       var middle = forms.generate({
+          view: 'generic',
+          resource: keys,
+          action: '/keys',
+          params: req.resource.params,
+          req: req,
+          res: res,
+          query: { owner: req.session.user },
+          useLayout: false,
+          form: {
+            legend: 'Create New Key',
+            submit: "Add Key",
+            showDestroyButton: true
+          },
+          schema: keySchema
+        }, function (err, result) {
+          if (err) {
+            return res.end(err.message);
+          }
+          $('.keysForm').html(result);
+
+          keys.find({ owner: req.session.user }, function (err, _keys) {
+
+            if (err) {
+              return res.end(err.message);
+            }
+            _keys = _keys.filter(function (item) {
+              if (item.key_type === "internal") {
+                // do not show interal api key to user
+                return false;
+              }
+              return true;
+            });
+            if (_keys.length === 0) {
+              $('.keyHolder').append('<h3>No API Keys Have Been Created</h3>')
+              return callback(null, $.html());
+            }
+            
+            if (req.session.serviceLimits) {
+              $('.keyUsage').html(_keys.length + '/' + req.session.serviceLimits.apiKeys);
+            }
+            _keys.forEach(function(k){
+              var table = '<table class="table well">';
+              var deleteLink = '<a class="destroyLink" data-name="' + k.name + '" href="?'  + 'destroy=true&id='  + k.id + '">' + 'destroy' + '</a>';
+              table += '<tr><td><strong>name: ' + '</strong>' + k.name + '</td>' + '<td><strong>hook_private_key:</strong> <input size="36" readonly="readonly" value="' + k.hook_private_key + '"/></td><td>' + deleteLink + '</td></tr>';
+              if (typeof k.roles === "string") {
+                k.roles = k.roles.split(',');
+              }
+              k.roles.sort();
+              if (typeof k.roles === 'object' && k.roles.length > 0) {
+                var str = '<ul class="list-inline">';
+                k.roles.forEach(function(r){
+                  var desc = '';
+                  if (role.roles[r] && role.roles[r].description) {
+                    desc = role.roles[r].description;
+                  }
+                  str += '<li class="roleItem" title="' + desc + '">' + r + '</li>';
+                });
+                str += '</ul>';
+                table += '<tr><td colspan="3">' + str + '</td>' + '</tr>';
+              }
+              table += '<table>';
+              $('.keyHolder').append(table);
+            });
+
+            forms.generate({
+                type: 'grid',
+                data: _keys,
+                action: '/keys',
+                params: req.resource.params,
+                req: req,
+                res: res,
+                query: { owner: req.session.user },
+                useLayout: false,
+                form: {
+                  legend: 'Your Keys',
+                  keys: ['name', 'hook_private_key', 'customRoles', 'roles'],
+                  showDestroyButton: true
+                },
+                schema: keySchema
+            }, function (err, _h) {
+              return callback(null, $.html());
+            });
+          });
+        });
+     }
+    });
  }
 
 };
