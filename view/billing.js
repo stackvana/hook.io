afc40e9988d7bdde14675b6350cd4dbe612f2798
Marak <marak.squires@gmail.com>
2019-03-17 10:28:46 -0400

    [view] [minor] Copy and trial date

--- view/billing.js
+++ view/billing.js
@@ -22,7 +22,7 @@ module['exports'] = function view (opts, callback) {
   var created = moment(req.session.user_ctime);
 
   if (req.session.servicePlan === 'free') {
-    created = moment('5/15/2018');
+    created = moment('3/17/2019');
   }
 
   var daysSinceCreation = now.diff(created, 'days');
