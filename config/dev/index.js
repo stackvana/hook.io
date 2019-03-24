43ef55f41a616b238752d13ca339c6eb68851a9d
Marak <marak.squires@gmail.com>
2019-03-24 14:46:53 -0400

    [dist] [refactor] [minor] Better config semantics

--- config/dev/index.js
+++ config/dev/index.js
@@ -1,6 +1,104 @@
+const app = {
+  name: "hook.io",
+  // host: "https://hook.io",
+  url: "http://localhost:9999",
+  ws: "ws://localhost:9999",
+  //url: "https://hook.io",
+  domain: "localhost",
+  port: "9999",
+  logo: "http://localhost:9999/img/logo.png",
+  logoInverse: "http://localhost:9999/img/logo-inverse.png",
+  adminEmail: "hookmaster@hook.io"
+};
+
+const redis = {
+  port: 6379,
+  password: "password",
+  host: "0.0.0.0"
+};
+
+const couch = {
+  "username": "admin",
+  "password": "password",
+  "port": 5984,
+  "host": "0.0.0.0"
+};
+
+const cluster = {
+  registerWithLoadBalancer: true,
+  pools : {
+    // Note: All services will auto-port up based on the first available port after the starting port
+    // this allows us define ranges of ports of the elastic pool instead of pre-configuring the pool statically
+    web: [],
+    worker: [],
+    lb: []
+  }
+};
+
+const balancer = {
+  port: 9999,
+  host: "0.0.0.0",
+  https: false,
+  publicIP: "127.0.0.1",
+  roots: ["hookio", "0.0.0.0", "localhost", "hook.io", "www.hook.io", "couch"],
+  secret: "change secret",
+  redis: {
+    prefix: '/session/',
+    port: redis.port,
+    password: redis.password,
+    host: redis.host
+  }
+};
+
+const broadcast = {
+  port: 9998,
+  host: "0.0.0.0",
+  https: false,
+  publicIP: "127.0.0.1",
+  roots: ["ws.hookio", "0.0.0.0", "localhost", "ws.hook.io"],
+  secret: "change secret",
+  redis: {
+    prefix: '/session/',
+    port: redis.port,
+    password: redis.password,
+    host: redis.host
+  }
+};
+
+const web = {
+  port: 11000,
+  host: "app",
+  https: false,
+  registerWithLoadBalancer: cluster.registerWithLoadBalancer,
+  roots: ["hookio", "0.0.0.0", "localhost", "hook.io", "www.hook.io"],
+  secret: "change secret",
+  redis: {
+    prefix: '/session/',
+    port: redis.port,
+    password: redis.password,
+    host: redis.host
+  }
+};
+
+const worker = {
+  host: '0.0.0.0',
+  startingPort: 10000,
+  registerWithLoadBalancer: cluster.registerWithLoadBalancer,
+  nproc: {
+    soft: 15000,
+    hard: 20000
+  },
+  //npmPath: __dirname + '/../../../../stackvana/microcule/' || "/Users/chroot/",
+  npmPath: "/var/chroot/root/microcule/",
+  publicIP: 'worker0',
+  chrootUser: 'worker',
+  useChroot: false,
+  useNSJAIL: false,
+  nsJailArgs: [ '-Mo', '--disable_clone_newnet', '--chroot', '/var/chroot/', '--user', '99999', '--group', '99999', '--rlimit_as', '9999', '--log', 'nsjail.log', '--rlimit_nproc', '1000' ,'--quiet', '--']
+};
+
 module['exports'] = {
   usingDocker: true,
-  dev: false,
   superadmin: {
     "name": "superadmin",
     "password": "footy",
@@ -14,71 +112,31 @@ module['exports'] = {
   pools : {
     // Note: All services will auto-port up based on the first available port after the starting port
     // this allows us define ranges of ports of the elastic pool instead of pre-configuring the pool statically
-    web: [],
-    worker: [],
-    lb: []
+    web: cluster.pools.web,
+    worker: cluster.pools.worker,
+    lb: cluster.pools.lb
   },
-  web: {
-    port: 11000,
-    host: "app",
-    https: false,
-    registerWithLoadBalancer: true,
-    roots: ["hookio", "0.0.0.0", "localhost", "hook.io", "www.hook.io"],
-    secret: "change secret",
-    redis: {
-      prefix: '/session/',
-      port: 6379,
-      password: "password",
-      host: "redis"
-    }
-  },
-  balancer: {
-    port: 9999,
-    host: "0.0.0.0",
-    https: false,
-    publicIP: "127.0.0.1",
-    roots: ["hookio", "0.0.0.0", "localhost", "hook.io", "www.hook.io", "couch"],
-    secret: "change secret",
-    redis: {
-      prefix: '/session/',
-      port: 6379,
-      password: "password",
-      host: "redis"
-    }
-  },
-  broadcast: {
-    port: 9998,
-    host: "0.0.0.0",
-    https: false,
-    publicIP: "127.0.0.1",
-    roots: ["ws.hookio", "0.0.0.0", "localhost", "ws.hook.io"],
-    secret: "change secret",
-    redis: {
-      prefix: '/session/',
-      port: 6379,
-      password: "password",
-      host: "redis"
-    }
-  },
-  //baseUrl: "http://localhost:9999",
+  web: web,
+  balancer: balancer,
+  broadcast: broadcast,
   baseUrl: "http://localhost:9999",
   couch: {
     "database": "hook",
     "type": "couch2",
-    "username": "admin",
-    "password": "password",
-    "port": 5984,
-    "host": "couch"
+    "username": couch.username,
+    "password": couch.password,
+    "port": couch.port,
+    "host": couch.host
   },
   redisCache: {
-    port: 6379,
-    password: "password",
-    host: "redis"
+    port: redis.port,
+    password: redis.password,
+    host: redis.host
   },
   redis: {
-    port: 6379,
-    password: "password",
-    host: "redis"
+    port: redis.port,
+    password: redis.password,
+    host: redis.host
   },
   github: {
     accessName: "",
@@ -108,25 +166,10 @@ module['exports'] = {
   chrootDirectory: '/var/chroot',
   //sslKeyDirectory: __dirname + '/../ssl/',
   //chrootDirectory: '/Users/worker',
-  chrootUser: 'worker',
-  useNSJAIL: false,
-  nsJailArgs: [ '-Mo', '--disable_clone_newnet', '--chroot', '/var/chroot/', '--user', '99999', '--group', '99999', '--rlimit_as', '9999', '--log', 'nsjail.log', '--rlimit_nproc', '1000' ,'--quiet', '--'],
-  useChroot: false,
   locales: {
     locales: ['en', 'de']
   },
-  worker: {
-    host: '0.0.0.0',
-    startingPort: 10000,
-    registerWithLoadBalancer: true,
-    nproc: {
-      soft: 15000,
-      hard: 20000
-    },
-    //npmPath: __dirname + '/../../../../stackvana/microcule/' || "/Users/chroot/",
-    npmPath: "/var/chroot/root/microcule/",
-    publicIP: 'worker0'
-  },
+  worker: worker,
   customDomains: false,
   MAX_SERVICE_EXECUTIONS_PER_CYCLE: Infinity,
   MAX_SERVICE_CONCURRENCY: 10,
@@ -136,16 +179,5 @@ module['exports'] = {
     serviceExecutionTimeout: require('../messages/serviceExecutionTimeout'),
     unauthorizedRoleAccess: require('../messages/unauthorizedRoleAccess')
   },
-  app: { // TODO: make able to white-label easily
-    name: "hook.io",
-    // host: "https://hook.io",
-    url: "http://localhost:9999",
-    ws: "ws://localhost:9999",
-    //url: "https://hook.io",
-    domain: "localhost",
-    port: "9999",
-    logo: "http://localhost:9999/img/logo.png",
-    logoInverse: "http://localhost:9999/img/logo-inverse.png",
-    adminEmail: "hookmaster@hook.io"
-  }
+  app: app
 };
