d53de66c9e971773f757297d3e7fe36f08ece1ad
Marak <marak.squires@gmail.com>
2019-03-25 09:25:26 -0400

    [view] [minor] [fix] Broken links

--- view/examples.js
+++ view/examples.js
@@ -8,49 +8,6 @@ module['exports'] = function (opts, callback) {
     if (err) {
       return res.end(err.message);
     }
-    var grouped = {};
-
-    var alpha = ['gcc', 'go', 'ocaml', 'rust', 'r', 'java'];
-
-    results = results.filter(function(h){
-      if (alpha.indexOf(h.language) !== -1) {
-        return false;
-      }
-      return true;
-    });
-
-    results.forEach(function(h) {
-      h.language = h.language || "javascript";
-      grouped[h.language] = grouped[h.language] || [];
-      grouped[h.language].push(h);
-    });
-
-    Object.keys(grouped).forEach(function(key){
-      // group headers per language
-      $('.examples').append('<a name="' + key + '"></a>');
-      $('.examples').append('<h3>' + key + '</h3>');
-
-      // update side menu
-      $('.langMenu').append('<li><a href="#' + key + '">' + key + '</a> </li>');
-
-      grouped[key].sort(function(a, b){
-          if(a.name < b.name) return -1;
-          if(a.name > b.name) return 1;
-          return 0;
-      })
-
-      grouped[key].forEach(function(h){
-        // TODO: add description of hook with h.description ( data is missing for most examples )
-        var serviceLink = '{{appUrl}}/examples/' + h.name + '';
-        $('.examples').append('<a href="' + serviceLink + '/_src"><span title="View Source" class="forkBtn octicon octicon-file-code"></span></a>&nbsp;<a href="' + serviceLink + '/fork"><span title="Fork Service" class="forkBtn octicon octicon-repo-forked"></span></a> <a href="' + serviceLink + '">' + h.name + '</a><br/>')
-      });
-      $('.examples').append('<br/>');
-      // TODO: group by language type
-      // if (h.name.search(/examples-/) !== -1) {
-        // $('.examples').append('<a href="{{appUrl}}/examples/' + h.name + '">' + h.name + '</a><br/>')
-      // }
-    });
-
 
     if (req.jsonResponse) {
       return res.json(results);
